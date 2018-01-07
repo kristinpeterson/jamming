3848ac548797e070f2b8a54f9833df320ca09ff6
kristinpeterson <kristinpeterson@me.com>
2018-01-06 17:02:26 -0800

    Add initial App styles and structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './images/logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <div className="App-playlist">
+          </div>
+        </div>
       </div>
     );
   }
