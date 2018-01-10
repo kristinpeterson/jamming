f04eeabacddcd8d4d0c3b4389391a586ad8cc545
kristinpeterson <kristinpeterson@me.com>
2018-01-09 22:41:56 -0800

    Feature: Enter key submits search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -9,6 +9,7 @@ class Search extends Component {
         };
         this.handleTermChange = this.handleTermChange.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleEnter = this.handleEnter.bind(this);
         if(this.state.term) {
             this.handleSearch();
         }
@@ -25,12 +26,19 @@ class Search extends Component {
         this.props.searchSpotify(this.state.term);
     }
 
+    handleEnter(e) {
+        if (e.key === 'Enter') {
+            this.handleSearch();
+        }   
+    }
+
     render() {
         return (
             <div className='Search'>
                 <input onChange={this.handleTermChange} 
+                        onKeyPress={this.handleEnter}
                         {...this.state.term ? {value: this.state.term} : {placeholder: 'Enter a Song, Album or Artist'}} />
-                <a onClick={this.handleSearch}>SEARCH</a>
+                <a onClick={this.handleSearch}>SEARCH &crarr;</a>
             </div>
         );
     }
